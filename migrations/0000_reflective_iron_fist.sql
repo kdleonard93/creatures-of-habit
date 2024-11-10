f2cc5c85e50739fa5616a4ecaa6a2435d0c2fbb9
Kyle <kdleonard@icloud.com>
2024-11-10 15:23:31 -0600

    initial db migration
    Still need to build out the schema a bit more for users

---
+++ migrations/0000_reflective_iron_fist.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `session` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `user` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`age` integer,
+	`username` text NOT NULL,
+	`password_hash` text NOT NULL,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `user_username_unique` ON `user` (`username`);
