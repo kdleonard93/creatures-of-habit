5443990d5086a4248980cdc180658dab95c61b1e
Kyle <kdleonard@icloud.com>
2024-11-10 21:04:05 -0600

    Added github workflow
    Slight update to the Header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -17,7 +17,7 @@
   </script>
   
   <header class="border-b">
-    <div class="container mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="container mx-auto px-2 sm:px-4 lg:px-6">
       <div class="flex h-16 items-center justify-between">
         <!-- Logo -->
         <div class="flex items-center">
