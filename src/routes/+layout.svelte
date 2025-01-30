72fa9f4868f6052c2e065026b06d42317123ba6a
Kyle <kdleonard@icloud.com>
2025-01-29 22:14:04 -0600

    layout.svelte update
    <slot /> is deprecated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,13 @@
 	import type { LayoutData } from './$types';
 
 	const props = $props<{ data: LayoutData }>();
+	const {children} = props;
 </script>
 
 <Header />
 
 <main class="container mx-auto px-4 py-8">
-	<slot />
+	{@render children?.()}
 </main>
 
 <Footer />
